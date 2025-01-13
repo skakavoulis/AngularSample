943e9e85c599df70a8b010d3ec73cb8ce7bd6bad
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2025-01-13 11:18:33 +0200

    Fix during class

--- src/app/components/route-standalone-component/route-standalone-component.component.ts
+++ src/app/components/route-standalone-component/route-standalone-component.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-route-standalone-component',
@@ -8,9 +9,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './route-standalone-component.component.css',
 })
 export class RouteStandaloneComponentComponent {
-  id: string;
+  id = '';
+  subscription?: Subscription;
 
-  constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.subscription = this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
   }
 }
